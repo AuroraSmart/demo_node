eacd06800e299094449326002ca68144c902efdd
Jan Heinsohn <jan.heinsohn@aurora-home.de>
2020-02-08 13:24:00 -0800

    Init

---
+++ main/mqtt_example.c
@@ -0,0 +1,162 @@
+// Copyright 2017 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mdf_common.h"
+#include "mwifi.h"
+#include "mesh_mqtt_handle.h"
+
+
+static const char *TAG = "mqtt_examples";
+
+static void node_read_task(void *arg)
+{
+    mdf_err_t ret = MDF_OK;
+    char *data    = MDF_MALLOC(MWIFI_PAYLOAD_LEN);
+    size_t size   = MWIFI_PAYLOAD_LEN;
+    mwifi_data_type_t data_type      = {0x0};
+    uint8_t src_addr[MWIFI_ADDR_LEN] = {0x0};
+
+    MDF_LOGI("Node read task is running");
+
+    for (;;) {
+        if (!mwifi_is_connected()) {
+            vTaskDelay(500 / portTICK_RATE_MS);
+            continue;
+        }
+
+        size = MWIFI_PAYLOAD_LEN;
+        memset(data, 0, MWIFI_PAYLOAD_LEN);
+        ret = mwifi_read(src_addr, &data_type, data, &size, portMAX_DELAY);
+        MDF_ERROR_CONTINUE(ret != MDF_OK, "<%s> mwifi_read", mdf_err_to_name(ret));
+        MDF_LOGD("Node receive: " MACSTR ", size: %d, data: %s", MAC2STR(src_addr), size, data);
+    }
+
+    MDF_LOGW("Node read task is exit");
+    MDF_FREE(data);
+    vTaskDelete(NULL);
+}
+
+static void node_write_task(void *arg)
+{
+    mdf_err_t ret = MDF_OK;
+    int count     = 0;
+    size_t size   = 0;
+    char *data    = NULL;
+    mwifi_data_type_t data_type     = {0x0};
+    uint8_t sta_mac[MWIFI_ADDR_LEN] = {0};
+
+    MDF_LOGI("Node task is running");
+
+    esp_wifi_get_mac(ESP_IF_WIFI_STA, sta_mac);
+
+    for (;;) {
+        if (!mwifi_is_connected() || !mwifi_get_root_status()) {
+            vTaskDelay(500 / portTICK_RATE_MS);
+            continue;
+        }
+
+        /**
+         * @brief Send device information to mqtt server throught root node.
+         */
+        size = asprintf(&data, "{\"mac\": \"%02x%02x%02x%02x%02x%02x\", \"seq\":%d,\"layer\":%d, \"message\": \"Hallo Fabian!\"}",
+                        MAC2STR(sta_mac), count++, esp_mesh_get_layer());
+
+        MDF_LOGD("Node send, size: %d, data: %s", size, data);
+        ret = mwifi_write(NULL, &data_type, data, size, true);
+        MDF_FREE(data);
+        MDF_ERROR_CONTINUE(ret != MDF_OK, "<%s> mwifi_write", mdf_err_to_name(ret));
+
+        vTaskDelay(3000 / portTICK_RATE_MS);
+    }
+
+    MDF_LOGW("Node task is exit");
+    vTaskDelete(NULL);
+}
+
+static mdf_err_t wifi_init()
+{
+    mdf_err_t ret          = nvs_flash_init();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        MDF_ERROR_ASSERT(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    MDF_ERROR_ASSERT(ret);
+
+    tcpip_adapter_init();
+    MDF_ERROR_ASSERT(esp_event_loop_init(NULL, NULL));
+    MDF_ERROR_ASSERT(esp_wifi_init(&cfg));
+    MDF_ERROR_ASSERT(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
+    MDF_ERROR_ASSERT(esp_wifi_set_mode(WIFI_MODE_STA));
+    MDF_ERROR_ASSERT(esp_wifi_set_ps(WIFI_PS_NONE));
+    MDF_ERROR_ASSERT(esp_mesh_set_6m_rate(false));
+    MDF_ERROR_ASSERT(esp_wifi_start());
+
+    return MDF_OK;
+}
+
+/**
+ * @brief All module events will be sent to this task in esp-mdf
+ *
+ * @Note:
+ *     1. Do not block or lengthy operations in the callback function.
+ *     2. Do not consume a lot of memory in the callback function.
+ *        The task memory of the callback function is only 4KB.
+ */
+static mdf_err_t event_loop_cb(mdf_event_loop_t event, void *ctx)
+{
+
+    switch (event) {
+
+        default:
+            break;
+    }
+
+    return MDF_OK;
+}
+
+void app_main()
+{
+    mwifi_init_config_t cfg   = MWIFI_INIT_CONFIG_DEFAULT();
+    mwifi_config_t config     = {
+        .mesh_id         = CONFIG_MESH_ID,
+        .mesh_password   = CONFIG_MESH_PASSWORD,
+        .mesh_type       = MWIFI_MESH_NODE
+    };
+
+    /**
+     * @brief Set the log level for serial port printing.
+     */
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+
+    /**
+     * @brief Initialize wifi mesh.
+     */
+    MDF_ERROR_ASSERT(mdf_event_loop_init(event_loop_cb));
+    MDF_ERROR_ASSERT(wifi_init());
+    MDF_ERROR_ASSERT(mwifi_init(&cfg));
+    MDF_ERROR_ASSERT(mwifi_set_config(&config));
+    MDF_ERROR_ASSERT(mwifi_start());
+
+    /**
+     * @brief Create node handler
+     */
+    //xTaskCreate(node_write_task, "node_write_task", 4 * 1024,
+            //    NULL, CONFIG_MDF_TASK_DEFAULT_PRIOTY, NULL);
+    xTaskCreate(node_read_task, "node_read_task", 4 * 1024,
+                NULL, CONFIG_MDF_TASK_DEFAULT_PRIOTY, NULL);
+}
